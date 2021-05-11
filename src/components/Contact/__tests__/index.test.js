6dd79bbb059a5af8fddb562557597546d8396ea7
Hilary Richardson <hilary.richardson@gmail.com>
2021-05-11 13:22:47 -0700

    updating contact form

---
+++ src/components/Contact/__tests__/index.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import ContactForm from '..';
+
+afterEach(cleanup);
+
+// renders test
+it('renders', () => {
+    render(<ContactForm
+        categories={categories}
+        setCurrentCategory={mockSetCurrentCategory}
+        currentCategory={mockCurrentCategory}
+        contactSelected={mockContactSelected}
+        setContactSelected={mockSetContactSelected}
+    />);
+})
--- src/components/Nav/__tests__/index.test.js
+++ src/components/Nav/__tests__/index.test.js
@@ -5,6 +5,26 @@ import Nav from '..';
 
 afterEach(cleanup);
 
+// mock functions for props
+const categories = [
+    { name: 'portraits', description: 'Portraits of people in my life' }
+]
+const mockCurrentCategory = jest.fn();
+const mockSetCurrentCategory = jest.fn();
+const mockContactSelected = jest.fn();
+const mockSetContactSelected = jest.fn();
+
+// renders test
+it('renders', () => {
+    render(<Nav
+        categories={categories}
+        setCurrentCategory={mockSetCurrentCategory}
+        currentCategory={mockCurrentCategory}
+        contactSelected={mockContactSelected}
+        setContactSelected={mockSetContactSelected}
+    />);
+})
+
 // test for general nav component
 describe('Nav component', () => {
     // baseline test
