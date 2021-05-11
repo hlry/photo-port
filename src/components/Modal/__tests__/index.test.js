5165ba33996e30418ae8c1079f120579e36680b1
Hilary Richardson <hilary.richardson@gmail.com>
2021-05-11 15:48:04 -0700

    adding modal and tests

---
+++ src/components/Modal/__tests__/index.test.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { render, cleanup, fireEvent } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+import Modal from '../'
+
+afterEach(cleanup)
+
+describe('Modal is rendering', () => {
+    // render test
+    it('renders', () => {
+        render(<Modal />);
+    });
+    // snapshot test
+    it('renders', () => {
+        const { asFragment } = render(<Modal />)
+        expect(asFragment()).toMatchSnapshot()
+    });
+});
+
+describe('Click Event', () => {
+    it('calls onClose handler', () => {
+        const { getByText } = render(<Modal
+            onClose={mockToggleModal}
+            currentPhoto={currentPhoto}
+        />);
+        fireEvent.click(getByText('Close this modal'));
+        expect(mockToggleModal).toHaveBeenCalledTimes(1);
+    });
+})
